e0ac144b0a7b3ad6a59728c562096ab163e69608
Julio Bermudez <jbermudez@distritomoda.com.ar>
2021-09-02 16:50:19 -0300

    FIRST COMMIT

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 4550
+const router = require('./routes/router')
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+app.use(express.json())
+app.use('/api',router)
+app.listen(port,()=>{
+    console.info(`server running on ${port}`)
+})
